898faca120f14f346880d7e5be531b91cc9a3c23
Paschalolo <pastechnology1@gmail.com>
2026-06-09 11:08:42 -0500

    message

--- src/intro_temp.cpp
+++ src/intro_temp.cpp
@@ -25,8 +25,26 @@ requires (ArithFlot_t<T>)
 T max(T a , T b ){
 	return a > b ? a : b ;	
 }
+ struct foo{
+	int a ; 
+	bool operator>(foo const & f){return a > f.a;}
+ };
+
+template <typename T> 
+int partition(T arr[] , int const low , int const high){
+	T pivot = arr[high] ; 
+	int i = (low -1);
+	for(int j = low ; j <= high -1; j++ ){
+		if(arr[j] < pivot){
+	i++; 
+	std::swap(&(arr[i], &(arr[j])));
+		}
+	}
+	return i +1 ;
+}
 
 int main(){
-	[[maybe_unused]] auto t = max(900U, 7.0f);
+	foo f1{}, f2{}; 
+	max<foo>(f1,f2);
 	return 0;
 }
